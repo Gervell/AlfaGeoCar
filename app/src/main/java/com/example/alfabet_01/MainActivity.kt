5ab2579f3c9b0990d72aa1916213850c6016d6a8
Rustammus <122292195+Rustammus@users.noreply.github.com>
2023-03-22 15:09:35 +0500

    beta 0.2
    Добавлен запрос на использование геолокации.
    Добавлена метка текущего местоположения

--- app/src/main/java/com/example/alfabet_01/MainActivity.kt
+++ app/src/main/java/com/example/alfabet_01/MainActivity.kt
@@ -2,7 +2,9 @@ package com.example.alfabet_01
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.app.ActivityCompat
 import com.yandex.mapkit.Animation
+import com.yandex.mapkit.MapKit
 import com.yandex.mapkit.MapKitFactory
 import com.yandex.mapkit.geometry.Point
 import com.yandex.mapkit.map.CameraPosition
@@ -21,9 +23,16 @@ class MainActivity : AppCompatActivity() {
         mapview = findViewById(R.id.mapview)
         mapview.map.move(CameraPosition(Point(56.838011, 60.597474), 11.0f, 0.0f, 0.0f))
         Animation(Animation.Type.SMOOTH, 300f)
-
+        var mapKit:MapKit = MapKitFactory.getInstance()
+        requestPerm()
+        var location = mapKit.createUserLocationLayer(mapview.mapWindow)
+        location.isVisible = true
     }
 
+    private fun requestPerm() {
+        ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION, android.Manifest.permission.ACCESS_COARSE_LOCATION), 102)
+        // Запрос на использование местоположения
+    }
     override fun onStop () {
         super.onStop()
         mapview.onStop()
