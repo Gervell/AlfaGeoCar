29205b75b8516858469825444eee27f4012107d3
Rustammus <122292195+Rustammus@users.noreply.github.com>
2023-05-23 19:33:46 +0500

    0.4.1

--- app/src/main/java/com/example/alfabet_01/DataHistoryAdapter.kt
+++ app/src/main/java/com/example/alfabet_01/DataHistoryAdapter.kt
@@ -1,11 +1,24 @@
 package com.example.alfabet_01
 
 import android.content.Intent
+<<<<<<< Updated upstream
+=======
+import android.util.Log
+>>>>>>> Stashed changes
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.example.alfabet_01.databinding.ParkinHistoryItemBinding
+<<<<<<< Updated upstream
+=======
+import java.text.DateFormat
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Locale
+
+>>>>>>> Stashed changes
 
 class DataHistoryAdapter: RecyclerView.Adapter<DataHistoryAdapter.DataHistoryHolder>() {
     val modelList = ArrayList<Model>()
@@ -13,12 +26,22 @@ class DataHistoryAdapter: RecyclerView.Adapter<DataHistoryAdapter.DataHistoryHol
         val binding = ParkinHistoryItemBinding.bind(item)
         fun bind(DataHistory: Model) {
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
             binding.textView9.text = DataHistory.text
 =======
             val date = Calendar.getInstance()
             date.time = DataHistory.date
             val time = DataHistory.text + date.get(Calendar.HOUR_OF_DAY).toString() + ":" + date.get(Calendar.MINUTE).toString()
             binding.textView9.text = time
+=======
+            binding.textView9.text = " ${DataHistory.text} \n" + DataHistory.dateString
+
+            binding.cardView1.setOnClickListener {
+                val intent = Intent(it.context, ParkingExtendedActivity::class.java)
+                intent.putExtra("ParkingData", DataHistory)
+                it.context.startActivity(intent)
+            }
+>>>>>>> Stashed changes
 
             binding.cardView1.setOnClickListener {
                 val intent = Intent(it.context, ParkinExtendedActivity::class.java)
@@ -45,6 +68,11 @@ class DataHistoryAdapter: RecyclerView.Adapter<DataHistoryAdapter.DataHistoryHol
 
     fun addModel (modArray: ArrayList<Model>) {
         modelList.addAll(modArray)
+        notifyDataSetChanged()
+    }
+
+    fun deleteAll () {
+        modelList.clear()
         notifyDataSetChanged()
     }
 }
--- app/src/main/java/com/example/alfabet_01/ParkinHistoryActivity.kt
+++ app/src/main/java/com/example/alfabet_01/ParkinHistoryActivity.kt
@@ -1,18 +1,33 @@
 package com.example.alfabet_01
 
 import android.content.Intent
+<<<<<<< Updated upstream:app/src/main/java/com/example/alfabet_01/ParkinHistoryActivity.kt
+=======
+import android.net.Uri
+>>>>>>> Stashed changes:app/src/main/java/com/example/alfabet_01/ParkingHistoryActivity.kt
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.alfabet_01.databinding.ActivityMainBinding
 import com.example.alfabet_01.databinding.ActivityParkinHistoryBinding
+<<<<<<< Updated upstream:app/src/main/java/com/example/alfabet_01/ParkinHistoryActivity.kt
 import com.yandex.mapkit.geometry.Point
+=======
+import java.io.File
+>>>>>>> Stashed changes:app/src/main/java/com/example/alfabet_01/ParkingHistoryActivity.kt
 
 class ParkinHistoryActivity : AppCompatActivity() {
 
+<<<<<<< Updated upstream:app/src/main/java/com/example/alfabet_01/ParkinHistoryActivity.kt
     lateinit var binding: ActivityParkinHistoryBinding
 
+=======
+    private lateinit var binding: ActivityParkinHistoryBinding
+    private val historyAdapter = DataHistoryAdapter()
+    var modelListGL  : ArrayList<Model>? = null
+>>>>>>> Stashed changes:app/src/main/java/com/example/alfabet_01/ParkingHistoryActivity.kt
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityParkinHistoryBinding.inflate(layoutInflater)
@@ -25,14 +40,21 @@ class ParkinHistoryActivity : AppCompatActivity() {
         } else {
             intent.getSerializableExtra("qwerty") as ArrayList<Model>?
         }
+        modelListGL = modelList
         if (modelList.isNullOrEmpty()) {
-            binding.recyclerHistory.visibility = View.INVISIBLE
-            binding.textView8.visibility = View.VISIBLE
+            setEmptyInvis()
         } else {
+<<<<<<< Updated upstream:app/src/main/java/com/example/alfabet_01/ParkinHistoryActivity.kt
 <<<<<<< Updated upstream:app/src/main/java/com/example/alfabet_01/ParkinHistoryActivity.kt
             historyAdapter.addModel(modelList!!)
             init()
 =======
+            historyAdapter.addModel(modelList)
+            binding.recyclerHistory.layoutManager = LinearLayoutManager(this)
+            binding.recyclerHistory.adapter = historyAdapter
+>>>>>>> Stashed changes:app/src/main/java/com/example/alfabet_01/ParkingHistoryActivity.kt
+=======
+            modelList.reverse()
             historyAdapter.addModel(modelList)
             binding.recyclerHistory.layoutManager = LinearLayoutManager(this)
             binding.recyclerHistory.adapter = historyAdapter
@@ -44,8 +66,55 @@ class ParkinHistoryActivity : AppCompatActivity() {
         binding.recyclerHistory.layoutManager = LinearLayoutManager(this)
         binding.recyclerHistory.adapter = historyAdapter
     }*/
+<<<<<<< Updated upstream:app/src/main/java/com/example/alfabet_01/ParkinHistoryActivity.kt
+=======
+    private fun setEmptyInvis() {
+        binding.recyclerHistory.visibility = View.INVISIBLE
+        binding.textView8.visibility = View.VISIBLE
+    }
+    fun onClickDeleteAll(view: View) {
+
+        if (!modelListGL.isNullOrEmpty()) {
+            historyAdapter.deleteAll()
+            for (i in modelListGL!!.indices) {
+                if (modelListGL!![i].imgPath != null) {
+                    fileDelete(modelListGL!![i].imgPath!!)
+                }
+            }
+            setEmptyInvis()
+            modelListGL = null
+        }
+    }
+
+    private fun fileDelete(uriString: String) {
+        Log.d("Tag", uriString)
+        val path = Uri.parse(uriString).path
+        Log.d("Tag", path!!)
+        val fdelete = File(path)
+        if (fdelete.exists()) {
+                if (fdelete.delete()) Log.d("Tag", "File delete")
+                else Log.d("Tag", "File not delete")
+        }
+        else Log.d("Tag", "File not exist")
+
+    }
+>>>>>>> Stashed changes:app/src/main/java/com/example/alfabet_01/ParkingHistoryActivity.kt
 
     fun onClickBack(view: View) {
+        val i = Intent()
+        i.putExtra("modelListFromHistory", modelListGL)
+        setResult(RESULT_OK, i)
         finish()
+    }
+
+    fun onClickAss(view: View) {
+        val path = binding.editTextText.text.toString()
+        Log.d("Tag", path)
+        val fdelete = File(path)
+        if (fdelete.exists()) {
+            if (fdelete.delete()) Log.d("Tag", "File delete")
+            else Log.d("Tag", "File not delete")
+        }
+        else Log.d("Tag", "File not exist")
     }
 }
