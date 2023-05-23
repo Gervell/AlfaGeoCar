379a7f66f24f4f5837e9fda394cf4019aa514774
Rustammus <122292195+Rustammus@users.noreply.github.com>
2023-05-23 14:41:25 +0500

    beta 0.4

--- app/src/main/java/com/example/alfabet_01/ParkinHistoryActivity.kt
+++ app/src/main/java/com/example/alfabet_01/ParkinHistoryActivity.kt
@@ -1,5 +1,6 @@
 package com.example.alfabet_01
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -11,13 +12,14 @@ import com.yandex.mapkit.geometry.Point
 class ParkinHistoryActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityParkinHistoryBinding
-    private var historyAdapter = DataHistoryAdapter()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityParkinHistoryBinding.inflate(layoutInflater)
         setContentView(binding.root)
         var modelList  : ArrayList<Model>? = null
-
+        val secondIntent = Intent(this, ParkinExtendedActivity::class.java)
+        val historyAdapter = DataHistoryAdapter()
         modelList = if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.TIRAMISU) {
             intent.getSerializableExtra("qwerty", ArrayList<Model>()::class.java)
         } else {
@@ -27,15 +29,21 @@ class ParkinHistoryActivity : AppCompatActivity() {
             binding.recyclerHistory.visibility = View.INVISIBLE
             binding.textView8.visibility = View.VISIBLE
         } else {
+<<<<<<< Updated upstream:app/src/main/java/com/example/alfabet_01/ParkinHistoryActivity.kt
             historyAdapter.addModel(modelList!!)
             init()
+=======
+            historyAdapter.addModel(modelList)
+            binding.recyclerHistory.layoutManager = LinearLayoutManager(this)
+            binding.recyclerHistory.adapter = historyAdapter
+>>>>>>> Stashed changes:app/src/main/java/com/example/alfabet_01/ParkingHistoryActivity.kt
         }
     }
 
-    private fun init() {
+    /*private fun init() {
         binding.recyclerHistory.layoutManager = LinearLayoutManager(this)
         binding.recyclerHistory.adapter = historyAdapter
-    }
+    }*/
 
     fun onClickBack(view: View) {
         finish()
